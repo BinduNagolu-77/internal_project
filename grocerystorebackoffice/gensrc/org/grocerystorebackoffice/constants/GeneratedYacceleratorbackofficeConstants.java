b5ab66bbb22cc0dc18920ecf688748171b7c9b96
Nagolu <bindu.nagolu@capgemini.com>
2023-12-06 10:46:44 +0530

    Backoffice commit

---
+++ grocerystorebackoffice/gensrc/org/grocerystorebackoffice/constants/GeneratedYacceleratorbackofficeConstants.java
@@ -0,0 +1,24 @@
+/*
+ * ----------------------------------------------------------------
+ * --- WARNING: THIS FILE IS GENERATED AND WILL BE OVERWRITTEN! ---
+ * --- Generated at 17-Nov-2023, 11:34:49 am                    ---
+ * ----------------------------------------------------------------
+ */
+package org.grocerystorebackoffice.constants;
+
+/**
+ * @deprecated since ages - use constants in Model classes instead
+ */
+@Deprecated(since = "ages", forRemoval = false)
+@SuppressWarnings({"unused","cast"})
+public class GeneratedYacceleratorbackofficeConstants
+{
+	public static final String EXTENSIONNAME = "grocerystorebackoffice";
+	
+	protected GeneratedYacceleratorbackofficeConstants()
+	{
+		// private constructor
+	}
+	
+	
+}
